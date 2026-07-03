c9fc2bfd8ef90c1197835cf9e49384e954d1b86c
lvdejesus <lvdejesus1@up.edu.ph>
2026-07-03 12:38:35 +0800

    add loop until done

--- calc.py
+++ calc.py
@@ -14,15 +14,20 @@ def div(x, y):
     answer = x / y
     return print(answer)
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-print("Operations: add, sub, mult, div")
-op = input("Enter operation: ")
-if op == "add":
-    add(num1, num2)
-elif op == "sub":
-    sub(num1, num2)
-elif op == "mult":
-    mult(num1, num2)
-elif op == "div":
-    div(num1, num2)
+done = False
+while not done:
+    print("Operations: add, sub, mult, div")
+    print("Type \"done\" if done.")
+    op = input("Enter operation: ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    if op == "add":
+        add(num1, num2)
+    elif op == "sub":
+        sub(num1, num2)
+    elif op == "mult":
+        mult(num1, num2)
+    elif op == "div":
+        div(num1, num2)
+    elif op == "done":
+        done = True
