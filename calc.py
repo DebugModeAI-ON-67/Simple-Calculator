74297fa303e7f3dc19b3bdbcfbaa75df9b65d184
Pan <axmamaradlo16@gmail.com>
2026-07-04 19:48:43 +0800

    Added and fixed a few things

--- calc.py
+++ calc.py
@@ -1,24 +1,30 @@
+import os
+
 def add(x, y):
     answer = x + y
-    return print(answer)
+    return print("\nSum: " + str(answer))
 
 def sub(x, y):
     answer = x - y
-    return print(answer)
+    return print("\nDifference: " + str(answer))
 
 def mult(x, y):
     answer = x * y
-    return print(answer)
+    return print("\nProduct: " + str(answer))
 
 def div(x, y):
     answer = x / y
-    return print(answer)
+    return print("\nQuotient: " + str(answer))
 
 done = False
 while not done:
+    print("================================")
+    print("Welcome to the simple calculator!")
     print("Operations: add, sub, mult, div")
     print("Type \"done\" if done.")
-    op = input("Enter operation: ")
+    op = input("\nEnter operation: ")
+    if op == "done":
+        break
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
     if op == "add":
@@ -29,7 +35,9 @@ while not done:
         mult(num1, num2)
     elif op == "div":
         div(num1, num2)
-    elif op == "done":
-        done = True
     else:
         print("Invalid operation!")
+    print("================================")
+
+    os.system('pause')
+    os.system('cls')
